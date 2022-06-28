464412afee1d6abb9ef3dbffb4ea0dcb47db2488
avutibhavana <avutibhavana06@gmail.com>
2022-06-28 21:28:13 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import './App.css'
-  
+
 function App() {
 
     const [posts, setPosts] = useState<any[]>([]);
@@ -28,18 +28,15 @@ function App() {
         <>
             <div className="App">         
                 
-                    {posts.map((item) =>
-                                           
+                    {posts.map((item) =>                                          
                         <div className='total'>   
-                        {/* <div><img className="image" src="https://tse1.mm.bing.net/th?id=OIP.pEdHkxmnEZGF_fYifpsW1gHaML&pid=Api&P=0&w=115&h=189"></img></div>                                 */}
+                        <div><img className="image" src={item.picture.large}></img></div>                                
                         <div className='fullname'><h4>Name:</h4><p>{item.name.title}{item.name.first}{item.name.last}</p></div>
                         <div className='email'><h4>Email:</h4><p>{item.email}</p></div>
+                        <div className='gender'><h4>Gender:</h4>{item.gender}</div>
                         <div><button onClick={resetFunc}>Refresh</button></div>                       
-                        </div>
-                      
-                        
-                      )}
-                
+                        </div>                                             
+                      )}    
             </div>
         </>
     );
